752e4a2fa16709ad5c0f848b7f067592bff76b78
Terry <547522203@qq.com>
2016-12-01 14:39:27 +0800

    Before appling datepicker

--- build/entry.js
+++ build/entry.js
@@ -1,10 +1,14 @@
-//js
-require("babel-polyfill");
+//css
 
+//js
+require('babel-polyfill');
+var r = require('rmc-date-picker');
+console.dir(r);
 
 // jsx
 require('./jsx/component.jsx');
 require('./jsx/composition.jsx');
  
+
 // sass
 require('./sass/app.scss');
--- build/jsx/component.jsx
+++ build/jsx/component.jsx
@@ -1,11 +1,11 @@
 import React,  { Component } from 'react';
 import {render} from 'react-dom';
-import { Button, Grid, Dropdown } from 'semantic-ui-react'
+import { Button, Grid, Dropdown, Checkbox, Form, Input, Segment } from 'semantic-ui-react'
 import observe from '../js/observe.js';
 let storekeeper = require('../js/storekeeper.js');
 let settings = storekeeper.settings;
 let tasks = storekeeper.tasks;
-
+	
 // test
 setTimeout(() => {
 	// tasks.push({name: 'task2', level: 'a'});
@@ -14,6 +14,77 @@ setTimeout(() => {
 	// settings.push({email: 'test1@testEmail.com'});
 }, 1000);
 
+
+
+// TaskInfo
+class TaskInfo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.modes = ['add', 'edit'];
+		this.state = {
+			mode: 'add'
+		};
+	}
+	render() {
+		const modes = this.modes;
+		const modesOptions = modes.map((item, index) => ({text: item, value: index}));
+		const selectValue = modes.indexOf('add');
+		return (
+			<div>
+				<div>
+					<Button className='BackBtn' icon='angle left'/>
+				</div>
+				<Grid>
+
+				    <Grid.Column></Grid.Column>
+				    <Grid.Column width={14}>
+				        <Form>
+				            <Form.Field inline>
+				                <Input className='AddTask_TaskNameInput' label='Name' placeholder='Type the task' fluid />
+				            </Form.Field>
+				            <Form.Field inline>
+				            	<Button content='Type' />
+				                <Dropdown className='TaskTypeSelector' defaultValue={selectValue}  selection options={modesOptions}></Dropdown>
+				            </Form.Field>
+				        </Form>
+				    </Grid.Column>
+				    <Grid.Column></Grid.Column>
+				  </Grid>
+				
+				
+
+			</div>);
+	}
+}
+
+// TaskListItem
+class TaskListItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			editMode: false
+		};
+	}
+	render() {
+		var task = this.props.task;
+		const content_normal =  <div>
+									<Checkbox className="CompleteBtn" />
+									<span className="TaskNameText">{task.name}</span>
+									<span className="Remark">Remark</span>
+								</div>;
+		const content_editMode = <div>
+									<Button basic icon='remove circle' className="DeleteBtn" />
+									<input className="Tasklist_TaskNameInput" defaultValue={task.name} />
+									<Button basic icon='content' className="SortBtn" />
+								</div>;
+		const content = this.state.editMode ? content_editMode : content_normal;
+		return( 
+			<div>
+				{content}
+				{this.props.children}
+			</div>);
+	}
+}
 
 // TaskList
 class TaskList extends React.Component {
@@ -37,9 +108,10 @@ class TaskList extends React.Component {
 
 	render() {
 		var tasks = this.state.tasks;
-		return <ul>
-			{tasks.map((task, index) => (<li key={index}>{task.level}&nbsp;&nbsp;{task.name}</li>))}
-		</ul>;
+		return (
+			<div>
+				{tasks.map((task, index) => (<TaskListItem key={index} task={task}/>))}
+			</div>);
 	}
 }
 
@@ -66,9 +138,10 @@ class TitleBar extends React.Component {
 	}
 	render() {
 		const taskTypes = this.props.taskTypes;
+		const taskTypesOptions = taskTypes.map((item, index) => ({text: item, value: index}));
 		const taskType = this.props.taskType;
 		const selectValue = taskTypes.indexOf(taskType) || 0;
-		const dropDown = <Dropdown fluid selection value={selectValue} options={taskTypes.map((item, index) => ({text: item, value: index}))}></Dropdown>
+		const dropDown = <Dropdown fluid selection defaultValue={selectValue} options={taskTypesOptions}></Dropdown>
 		var singleText = <p>{taskTypes[0]}</p>
 		const showContent = taskTypes.length > 1 ? dropDown : singleText;
 		return (
@@ -111,13 +184,22 @@ class LongTaskContainer extends React.Component {
 	}
 }
 
+// MultiFunctionBtn
+class MultiFunctionBtn extends React.Component {
+	render() {
+		const taskTypes = ['long', 'year', 'month', 'week'];
+		return <button>MultiFunctionBtn</button>;
+	}
+}
+
 
 // ToDoList
 class ToDoList extends React.Component {
 	render() {
 		return (
 			<div>
-				<Grid>
+			    <TaskInfo />
+				{/*<Grid>
 				    <Grid.Row>
 				      <Grid.Column width={8}>
 				        <LongTaskContainer />
@@ -126,7 +208,8 @@ class ToDoList extends React.Component {
 				        <DayTaskContainer />
 				      </Grid.Column>
 				    </Grid.Row>
-				  </Grid>
+				</Grid>
+				<MultiFunctionBtn />*/}
 			</div>
 			);
 	}
