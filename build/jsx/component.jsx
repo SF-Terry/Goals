9771e6d8f3fc9a67330add41ebed92dbcf38e3c5
Terry <547522203@qq.com>
2016-12-19 15:10:11 +0800

    Remove sortable list fragment

--- build/jsx/component.jsx
+++ build/jsx/component.jsx
@@ -1001,49 +1001,18 @@ class TaskListItem extends React.Component {
  * @receiveProps {bool} isTaskCompleted - isTaskCompleted
  * @receiveProps {bool} editMode - editMode
  */
-/*function reinsert(arr, from, to) {
-  const _arr = arr.slice(0);
-  const val = _arr[from];
-  _arr.splice(from, 1);
-  _arr.splice(to, 0, val);
-  return _arr;
-}
-function clamp(n, min, max) {
-  return Math.max(Math.min(n, max), min);
-}
-const springConfig = {stiffness: 300, damping: 50};
-const itemsCount = 10;*/
 class TaskList extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			isShowTaskLists: true,
-			// sortable list
-			// topDeltaY: 0,
-			// mouseY: 0,
-			// isPressed: false,
-			// originalPosOfLastPressed: 0,
-			// order: range(itemsCount)
+			isShowTaskLists: true
 		}
 
 		this.taskListItemCallback = this.taskListItemCallback.bind(this);
-
-		// sortablel list
-		this.handleTouchStart = this.handleTouchStart.bind(this);
-		this.handleTouchMove = this.handleTouchMove.bind(this);
-		this.handleMouseDown = this.handleMouseDown.bind(this);
-		this.handleMouseMove = this.handleMouseMove.bind(this);
-		this.handleMouseUp = this.handleMouseUp.bind(this);
 	}
 
-	// sortable list
 	componentDidMount() {
-	    window.addEventListener('touchmove', this.handleTouchMove);
-	    window.addEventListener('touchend', this.handleMouseUp);
-	    window.addEventListener('mousemove', this.handleMouseMove);
-	    window.addEventListener('mouseup', this.handleMouseUp);
-
 	    // update list realtime in real time
 	    window.setInterval(() => {
 	    	this.setState({
@@ -1079,47 +1048,9 @@ class TaskList extends React.Component {
 		}
 	}
 
-	// sortable list
-	handleTouchStart(key, pressLocation, e) {
-	    this.handleMouseDown(key, pressLocation, e.touches[0]);
-	}
-	handleTouchMove(e) {
-	    e.preventDefault();
-	    this.handleMouseMove(e.touches[0]);
-	}
-	handleMouseDown(pos, pressY, {pageY}) {
-	    this.setState({
-	        topDeltaY: pageY - pressY,
-	        mouseY: pressY,
-	        isPressed: true,
-	        originalPosOfLastPressed: pos,
-	    });
-	}
-	handleMouseMove({pageY}) {
-	    const {isPressed, topDeltaY, order, originalPosOfLastPressed} = this.state;
-	
-	    if (isPressed) {
-	        const mouseY = pageY - topDeltaY;
-	        const currentRow = clamp(Math.round(mouseY / 100), 0, itemsCount - 1);
-	        let newOrder = order;
-	  
-	        if (currentRow !== order.indexOf(originalPosOfLastPressed)){
-	          newOrder = reinsert(order, order.indexOf(originalPosOfLastPressed), currentRow);
-	        }
-	  
-	        this.setState({mouseY: mouseY, order: newOrder});
-	    }
-	}
-	handleMouseUp() {
-	    this.setState({isPressed: false, topDeltaY: 0});
-	}
-
 	render() {
 		const {taskType, isTaskCompleted, editMode} = this.props;
 		const {isShowTaskLists} = this.state;
-
-		// sortable list
-		const {mouseY, isPressed, originalPosOfLastPressed, order} = this.state;
 
 		const filterdTasks = tasks.filter(task => {
 			const {taskType: t, isTaskCompleted: c} = task;
@@ -1129,52 +1060,12 @@ class TaskList extends React.Component {
 
 
 		return (
-				<div>
+				<div className='TaskList'>
 					{isShowTaskLists && isfilterdTasksNotEmpty ? (
 						<Menu fluid vertical>
 							{filterdTasks.map((task, index) => (<TaskListItem key={index} task={task} editMode={editMode} taskListItemCallback={this.taskListItemCallback}/>))}
 						</Menu>
 					) : null}
-
-					{/* sortable list */}
-					{/*<div className="demo8">
-					    {range(itemsCount).map(i => {
-					        const style = originalPosOfLastPressed === i && isPressed
-					              ? {
-					                    scale: spring(1.1, springConfig),
-					                    shadow: spring(16, springConfig),
-					                    y: mouseY,
-					                }
-					              : {
-					                    scale: spring(1, springConfig),
-					                    shadow: spring(1, springConfig),
-					                    y: spring(order.indexOf(i) * 100, springConfig),
-					                };
-					        return (
-					            <Motion style={style} key={i}>
-					                {({scale, shadow, y}) =>
-					                    <div
-					                        className="demo8-item"
-					                        style={{
-					                          boxShadow: `rgba(0, 0, 0, 0.2) 0px ${shadow}px ${2 * shadow}px 0px`,
-					                          transform: `translate3d(0, ${y}px, 0) scale(${scale})`,
-					                          WebkitTransform: `translate3d(0, ${y}px, 0) scale(${scale})`,
-					                          zIndex: i === originalPosOfLastPressed ? 99 : i,
-					                        }}>
-					                        {order.indexOf(i) + 1}
-					      
-					                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					                        Num: {i}
-					                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					                         
-					                        <Icon name='list layout' onMouseDown={this.handleMouseDown.bind(null, i, y)}
-					                        onTouchStart={this.handleTouchStart.bind(null, i, y)}/>
-					                  </div>
-					                }
-					            </Motion>
-					        );
-					    })}
-					</div>*/}
 				</div>
 			);
 	}
@@ -1291,7 +1182,7 @@ class TaskListContainer extends React.Component {
 		const defalutIsComplete = this.props.isCompleted;
 
 		return (
-			<div>
+			<div className='TaskListContainer'>
 				<Grid padded>
 					<Row>
 						<Column width={8} verticalAlign='middle'>
@@ -1466,7 +1357,7 @@ class MultiFunctionBtn extends React.Component {
 		this.setState({
 			showMailButton: true
 		});
-			
+
 		copyToClipboard(dom, (b) => {
 			if (b) {
 				observe_message.setting = {
